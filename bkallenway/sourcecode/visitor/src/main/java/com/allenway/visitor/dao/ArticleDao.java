f3602601a2047b8b5daf64a8fbbf87187fc20903
pzxwhc <wuhuachuan712@163.com>
2016-11-23 13:20:31 +0800

    UPDATE article order by title

--- bkallenway/sourcecode/visitor/src/main/java/com/allenway/visitor/dao/ArticleDao.java
+++ bkallenway/sourcecode/visitor/src/main/java/com/allenway/visitor/dao/ArticleDao.java
@@ -18,14 +18,14 @@ public interface ArticleDao extends JpaRepository<Article, String> {
     @Query(value = "select * from tb_article order by RAND() LIMIT ?1 order by operationTime",nativeQuery = true)
     List<Article> findRandomArticle(final int size);
 
-    @Query("select article.id,article.title,article.tagId from Article article where moduleId=:moduleId and isDelete=:isDelete order by operationTime")
+    @Query("select article.id,article.title,article.tagId from Article article where moduleId=:moduleId and isDelete=:isDelete order by title")
     Page<Object> findByModuleIdAndInPage(Pageable pageable,
                                           final @Param("moduleId") String moduleId,
                                           final @Param("isDelete") boolean isDelete);
     /**
      *  查找某 tagId 下的所有文章（分页）给游客使用
      */
-    @Query("select article.id,article.title,article.tagId from Article article where tagId=:tagId and isDelete=:isDelete order by operationTime")
+    @Query("select article.id,article.title,article.tagId from Article article where tagId=:tagId and isDelete=:isDelete order by title")
     Page<Object> findByTagIdAndInPage(Pageable pageable,
                                        final @Param("tagId") String tagId,
                                        final @Param("isDelete") boolean isDelete);
@@ -33,7 +33,7 @@ public interface ArticleDao extends JpaRepository<Article, String> {
     /**
      *  查找某 tagId 下的所有文章（分页）给管理员使用
      */
-    @Query("select article.id,article.title,article.tagId,article.isDelete from Article article where tagId=:tagId order by operationTime")
+    @Query("select article.id,article.title,article.tagId,article.isDelete from Article article where tagId=:tagId order by title")
     Page<Object> findByTagIdAndInPageForAdmin(Pageable pageable,
                                                final @Param("tagId") String tagId);
 
